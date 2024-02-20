ad6a12cefb42f4492aadb34d23ec6e917ac2bcbf
Vishal Maharathy <vishalmaharathy76@gmail.com>
2024-02-20 08:47:22 +0530

    firstPubSubModel

---
+++ publish.js
@@ -0,0 +1,34 @@
+/**
+ * TODO(developer): Uncomment these variables before running the sample.
+ */
+const dotenv = require('dotenv');
+dotenv.config();
+const topicNameOrId = 'testTopic';
+const data = JSON.stringify({success:true, message:'Hello, World!'});
+
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub({keyFilename: './key.json'});
+
+let messageCount = 0;
+async function publishMessage(topicNameOrId, data) {
+  // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+  const dataBuffer = Buffer.from(data);
+
+  try {
+    // setInterval(async() => {
+      let data = JSON.stringify({success:true, message:'Hello, World!', messageCount: messageCount++});
+      const dataBuffer = Buffer.from(data);
+      const messageId = await pubSubClient
+        .topic(topicNameOrId)
+        .publishMessage({data: dataBuffer});
+      console.log(`Message ${messageId} published.`);
+    // }, 5000);
+  } catch (error) {
+    console.error(`Received error while publishing: ${error.message}`);
+    process.exitCode = 1;
+  }
+}
+publishMessage(topicNameOrId, data);
